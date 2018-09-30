eed15299148738b771d545693b71e1127d5a8158
Bjørnar Luteberget <luteberget@gmail.com>
2018-09-30 21:08:53 +0200

    vis-rs: json export, compatible with docs, can now replace the other schematic solver

---
+++ src/json.rs
@@ -0,0 +1,123 @@
+use failure::Error;
+use solver::SolverOutput;
+use convert::OrigEdges;
+use std::collections::HashMap;
+use serde_json;
+
+type Pt = (f64,f64);
+type Line = (Pt,Pt);
+
+pub fn javascript_output(o :&SolverOutput, orig :&OrigEdges, node_names :&HashMap<String,usize>) -> Result<String, Error> {
+
+    let mut json = json!({});
+
+    let mut node_idxs = HashMap::new();
+    for (k,v) in node_names.into_iter() {
+        node_idxs.insert(v,k);
+    }
+
+    // Each edge should have a list of nodes and distances 
+    // example: [("n1a-n1b",200.0),...]
+
+    for (p1,p2,y) in &o.edge_levels {
+        println!("\n\nEDGE");
+        let (_,x1,y1) = &o.node_coords[p1.node];
+        let (_,x2,y2) = &o.node_coords[p2.node];
+        println!("{:?} -- {:?}", &o.node_coords[p1.node],
+                                 &o.node_coords[p2.node]);
+        let out_length = (*y as isize - *y1 as isize).abs();
+        let in_length = (*y2 as isize - *y as isize).abs();
+
+        let mut lines = Vec::new();
+        lines.push(((*x1,*y1 as f64),(x1+out_length as f64,*y as f64)));
+        lines.push(((*x1+out_length as f64, *y as f64),(*x2-in_length as f64,*y as f64)));
+        lines.push(((*x2-in_length as f64, *y as f64),(*x2, *y2 as f64)));
+        println!(" LINES out={} in={} {:?}", out_length, in_length, lines);
+        if out_length == 0 { lines.remove(0); }
+        if out_length as f64 + in_length as f64 + 1e-9 > (x2-x1) {
+            lines.remove(if  out_length == 0  { 0 } else {1 } ); // Should not happen
+            // TODO: panic!("Edge has no space on its level");
+        }
+        if in_length == 0 { let i = lines.len()-1; lines.remove(i); }
+        println!(" LINES {:?}", lines);
+
+        let km_length = x2-x1;
+        let mut pos = 0.0;
+        //for k in orig_edges {
+        //...
+        //}
+
+        let edge_id = ((node_idxs[&p1.node].clone(), p1.port),
+                       (node_idxs[&p2.node].clone(), p2.port));
+        let orig_edges = &orig[&edge_id];
+
+        let total :f64 = orig_edges.iter().map(|(_a,_b,d)| *d).sum();
+        let mut cum :f64 = 0.0;
+
+        for (a,b,d) in orig_edges {
+            if ! (*d > 0.0) { continue; }
+            let x1_rel = cum     / total;
+            let x2_rel = (cum+d) / total;
+            println!("oe:::{:?} {} {}", &lines, x1_rel, x2_rel);
+            let lines = get_slice(&lines, x1_rel, x2_rel);
+            println!("res::{:?}", lines);
+            cum += d;
+
+            let lines : Vec<serde_json::Value> = lines.iter().map(|(s,e)| json!([[s.0,s.1],[e.0,e.1]])).collect();
+
+            println!("line name {}-{}",a,b);
+            json.as_object_mut().unwrap().insert(format!("{}-{}", a, b), 
+                 json!({"length": d, 
+                        "lines": lines}));
+            use std::fmt::Write;
+            //writeln!(&mut json, "halla");
+        }
+
+        println!("edge from {:?} to {:?} through y={:?}", (x1,y1),(x2,y2),y);
+        println!("  out {} in  {}", out_length, in_length);
+        println!("  lines: {:?}", lines);
+
+    }
+
+
+    Ok(format!("var edges = {};",serde_json::to_string_pretty(&json).unwrap()))
+}
+
+fn lerp(a: f64, b :f64, p :f64) -> f64 { (1.0-p)*a + p*b }
+
+fn get_slice(lines :&[Line], a:f64, b:f64) -> Vec<Line> {
+    let km_start = (lines[0].0).0;
+    let km_length = (lines[lines.len()-1].1).0 - (lines[0].0).0;
+    let to_km = |x| (x-km_start)/km_length;
+    println!("get slice km={}", km_length);
+
+    let mut out : Vec<Line> = Vec::new();
+    for ((x1,y1),(x2,y2)) in lines.iter() {
+        let start_rel = to_km(x1);
+        let end_rel   = to_km(x2);
+
+        println!(" sliceline {} {} {} {}", a,b,start_rel,end_rel);
+
+        if end_rel < a { continue; }
+        if start_rel > b { continue; }
+
+        let mut start = (*x1,*y1);
+        let mut end =   (*x2,*y2);
+
+        if start_rel < a {
+            let a_in_interval = (a - start_rel)/(end_rel-start_rel);
+            start = (lerp(*x1,*x2, a_in_interval), 
+                     lerp(*y1,*y2, a_in_interval));
+        }
+        if end_rel > b {
+            let b_in_interval = (b - start_rel)/(end_rel-start_rel);
+            end = (lerp(*x1,*x2, b_in_interval), 
+                   lerp(*y1,*y2, b_in_interval));
+        }
+
+        out.push((start,end));
+    }
+
+
+    out
+}
