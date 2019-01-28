477a101ba8b29c14800c39866d391baa4c285e72
Bjørnar Luteberget <luteberget@gmail.com>
2019-01-28 14:14:39 +0100

    ci: missing cross-compilation linux packages

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@ case $TRAVIS_OS_NAME in
     linux)
         sudo apt update
         sudo apt install -y zlib1g-dev
-        sudo apt install -y libc6-dev-i386
+        sudo apt install -y g++-multilib gcc-multilib
         ;;
 esac
 
