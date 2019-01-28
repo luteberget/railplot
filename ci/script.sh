9b4376f7856b8dac3b1734430eabb6adda48dbd7
Bjørnar Luteberget <luteberget@gmail.com>
2019-01-28 12:14:13 +0100

    ci: avoid run error

--- ci/script.sh
+++ ci/script.sh
@@ -6,5 +6,5 @@ cargo build --target $TARGET
 cargo build --target $TARGET --release
 cargo test --target $TARGET
 cargo test --target $TARGET --release
-cargo run --target $TARGET
-cargo run --target $TARGET --release
+cargo run --target $TARGET -- --help
+cargo run --target $TARGET --release -- --help
